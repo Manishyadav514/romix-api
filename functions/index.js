0e7e4c7de4d1e01c0567700897685e6cf1093692
Manish <manish.yadav.elit@gmail.com>
2022-10-01 15:47:37 +0530

    test-route

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
 const functions = require("firebase-functions");
+const express = require("express");
+// const cors = require("cors");
+// const admin = require("firebase-admin");
+const app = express();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +11,10 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// ROUTES
+app.get("/", (req, res) => {
+  return res.status(200).send({data: "Hello World!"});
+});
+
+exports.app = functions.https.onRequest(app);
