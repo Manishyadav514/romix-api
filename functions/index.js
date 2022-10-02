33a116efbd437382f24129169f7b2ffdf9cb6d05
Manish <manish.yadav.elit@gmail.com>
2022-10-02 08:47:06 +0530

    POST request

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,41 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 const express = require("express");
-// const cors = require("cors");
-// const admin = require("firebase-admin");
 const app = express();
+const cors = require("cors");
+// add app to use cors
+app.use(cors({ origin: true }));
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Link Database
+const serviceAccount = require("./serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://romix-api.firebaseio.com",
+});
+const db = admin.firestore();
 
-// ROUTES
-app.get("/", (req, res) => {
-  return res.status(200).send({data: "Hello World!"});
+// ROUTES---------------------------------------
+
+// GET
+app.get("/test-data", (req, res) => {
+  return res.status(200).send({ data: "Hello World!" });
+});
+
+// POST
+app.post("/test-data", (req, res) => {
+  (async () => {
+    try {
+      await db.collection("test-data").doc("UID").set({
+        title: req.body.title,
+        description: req.body.description,
+        feeling: req.body.feeling,
+      });
+      return res.status(200).send("Your Response has been saved");
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
 });
 
 exports.app = functions.https.onRequest(app);
